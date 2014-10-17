7cfcd53e69f8cab434a119b88eb98c4771f90b52
GurKur <GurKur@mail.ru>
2014-10-17 10:50:39 +0400

    punkt 2.5.1 add diff

--- lr3_1.3.1.cpp
+++ lr3_1.3.1.cpp
@@ -2,11 +2,13 @@
 
 int main()
 {
-    int a,b,sum;
+    int a,b,sum,raz;
 cout <<"Enter a: \n";
 cin >>a;
 cout <<"Enter b: \n";
 cin >>b;
 sum=a+b;
 cout<<"sum="<<sum<<"\n";
+raz=a-b;
+cout<<"raz="<<raz<<"\n";
 }
